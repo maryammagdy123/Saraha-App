7fc39af6c1861bd354131b1a6583863645c6ee31
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-19 22:12:22 +0200

    fix otpRouter route path

--- src/app.bootstrap.js
+++ src/app.bootstrap.js
@@ -7,6 +7,7 @@ import {
   authRouter,
   messageRouter,
   userRouter,
+  otpRouter
 } from "./Modules/index.js";
 import { globalErrorHandling } from "./Middleware/error.middleware.js";
 
@@ -19,7 +20,7 @@ const bootstrap = async () => {
     res.json("Assignment-9!");
   });
   app.use("/api/auth", authRouter);
-  app.use("api/account", otpRouter);
+  app.use("/api/account", otpRouter);
   app.use("/api/user", userRouter);
   app.use("/api/message", messageRouter);
   app.all("{/*dummy}", async (req, res, next) => {
