d6a6dd1a810f60a1d7d8d632941b3fb204f114f0
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-19 19:45:43 +0200

    add verify email endpoint to verify account

--- src/Modules/OTP/otp.routes.js
+++ src/Modules/OTP/otp.routes.js
@@ -1,5 +1,5 @@
-// import { Router } from "express";
-// import * as controller from "./otp.controller";
-// const router = Router();
-// router.post("/verify-email", controller.verifyEmail);
-// export default router;
+import { Router } from "express";
+import * as controller from "./otp.controller";
+const router = Router();
+router.post("/verify-email", controller.verifyEmail);
+export default router;
