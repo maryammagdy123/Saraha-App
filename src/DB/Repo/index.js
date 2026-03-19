e6dad5a05da96a2c0c0cb1179df1b62021797d54
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-19 15:48:02 +0200

    feat: implement OTP repository with reusable DBRepository abstraction

--- src/DB/Repo/index.js
+++ src/DB/Repo/index.js
@@ -1,5 +1,6 @@
-import { MessageModel, UserModel } from "../Models/index.js";
+import { MessageModel, OtpModel, UserModel } from "../Models/index.js";
 import { DBRepository } from "./db.repository.js";
 
 export const userRepo = new DBRepository(UserModel);
 export const messageRepo = new DBRepository(MessageModel);
+export const otpRepo = new DBRepository(OtpModel);
