80d35bf713d88c971b8f13686a1ab85b2be7564a
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-20 00:16:14 +0200

    (faet) implement resend otp controller

--- src/Modules/OTP/otp.controller.js
+++ src/Modules/OTP/otp.controller.js
@@ -2,12 +2,26 @@ import { successResponse } from "../../Utils/Response/success.response.js";
 import * as service from "./otp.services.js";
 export const verifyEmail = async (req, res, next) => {
   try {
-    const verified=await service.verifyOTP(req.body, "verify");
+    const verified = await service.verifyOTP(req.body, "verify");
     return successResponse({
       res,
       status: 200,
       message: "Your Account verified successfully !",
-      data:{verified:verified.acknowledged}
+      data: { verified: verified.acknowledged },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const resendOTP = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    await service.resendOTP(email);
+    return successResponse({
+      res,
+      status: 200,
+      message: "Please check your email to get OTP",
     });
   } catch (error) {
     next(error);
