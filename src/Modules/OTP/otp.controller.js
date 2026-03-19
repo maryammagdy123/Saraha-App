f1621efa8cb87961a2ab6f18df023de9633259a6
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-19 20:36:01 +0200

    (feat) implement verifyOtp service

--- src/Modules/OTP/otp.controller.js
+++ src/Modules/OTP/otp.controller.js
@@ -1,9 +1,9 @@
 import { successResponse } from "../../Utils/Response/success.response.js";
-
+import * as service from "./otp.services.js";
 export const verifyEmail = async (req, res, next) => {
   try {
     const { email, otp } = req.body;
-    await service.verifyOTP(email, otp);
+    await service.verifyOTP(otp, email, "verify");
     return successResponse({
       res,
       status: 200,
--- src/Modules/OTP/otp.services.js
+++ src/Modules/OTP/otp.services.js
@@ -1,6 +1,11 @@
-import { otpRepo } from "../../DB/Repo/index.js";
+import { otpRepo, userRepo } from "../../DB/Repo/index.js";
 import { sendOTPEmail } from "../../Utils/email.utils.js";
-import { hash } from "../../Utils/index.js";
+import {
+  BadRequestException,
+  compare,
+  hash,
+  NotFoundException,
+} from "../../Utils/index.js";
 import { generateOTP } from "../../Utils/otp.utils.js";
 
 export const generateAndSendOTP = async (email, type) => {
@@ -12,7 +17,37 @@ export const generateAndSendOTP = async (email, type) => {
     otpType: type,
     expiresAt: Date.now() + 5 * 60 * 1000,
   });
-  await sendOTPEmail(email, otp, "Your Account OTP  verification code , note that otp will expire in 5 munites");
+  await sendOTPEmail(
+    email,
+    otp,
+    "Your Account OTP  verification code , note that otp will expire in 5 munites",
+  );
   console.log(otpDoc);
   return otpDoc;
+};
+
+export const verifyOTP = async (otp, email, type) => {
+  //check if user has an otp
+  const otpDoc = otpRepo.findOne({ filter: { email, type } });
+  const isCompare = await compare(otp, otpDoc.otp);
+  if (!otpDoc) {
+    BadRequestException({ message: "OTP has expired" });
+  }
+
+  //check if the otp entered by user matches the one in the db
+  if (!isCompare) {
+    BadRequestException({ message: "Invalid OTP" });
+  }
+
+  const user = await userRepo.find({
+    filter: {
+      email,
+    },
+  });
+  if (!user) {
+    NotFoundException({ message: "User Not found" });
+  }
+  user.isConfirmed = true;
+  await user.save();
+  return user;
 };
