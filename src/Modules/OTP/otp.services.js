3e5381d17df772a81d565d87c99ac98ccd50b5cd
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-19 22:18:21 +0200

    refactor otp services

--- src/Modules/OTP/otp.services.js
+++ src/Modules/OTP/otp.services.js
@@ -4,9 +4,9 @@ import {
   BadRequestException,
   compare,
   hash,
-  NotFoundException,
 } from "../../Utils/index.js";
 import { generateOTP } from "../../Utils/otp.utils.js";
+
 
 export const generateAndSendOTP = async (email, type) => {
   const otp = generateOTP();
@@ -15,39 +15,39 @@ export const generateAndSendOTP = async (email, type) => {
     email,
     otp: hashedOtp,
     otpType: type,
-    expiresAt: Date.now() + 5 * 60 * 1000,
+    expiresAt: Date.now() + 10 * 60 * 1000,
   });
   await sendOTPEmail(
     email,
     otp,
-    "Your Account OTP  verification code , note that otp will expire in 5 munites",
+    "Your Account OTP  verification code , note that otp will expire in 10 minutes",
   );
   console.log(otpDoc);
   return otpDoc;
 };
 
-export const verifyOTP = async (otp, email, type) => {
+export const verifyOTP = async (body, type) => {
   //check if user has an otp
-  const otpDoc = otpRepo.findOne({ filter: { email, type } });
-  const isCompare = await compare(otp, otpDoc.otp);
+  const otpDoc = await otpRepo.findOne({
+    filter: { email: body.email, otpType: type },
+  });
+
   if (!otpDoc) {
     BadRequestException({ message: "OTP has expired" });
   }
+  const isCompare = await compare(body.otp, otpDoc.otp);
 
   //check if the otp entered by user matches the one in the db
   if (!isCompare) {
     BadRequestException({ message: "Invalid OTP" });
   }
 
-  const user = await userRepo.find({
-    filter: {
-      email,
-    },
+  const updatedUser = await userRepo.updateOne({
+    filter: { email: body.email },
+    update: { isConfirmed: true },
   });
-  if (!user) {
-    NotFoundException({ message: "User Not found" });
+  if (updatedUser) {
+    await otpRepo.deleteOne({ email: body.email });
   }
-  user.isConfirmed = true;
-  await user.save();
-  return user;
+  return updatedUser;
 };
--- src/Modules/index.js
+++ src/Modules/index.js
@@ -2,4 +2,4 @@ import authRouter from "./Auth/auth.routes.js";
 import userRouter from "./User/user.routes.js";
 import messageRouter from "./Message/message.routes.js";
 import otpRouter from"./OTP/otp.routes.js"
-export { authRouter, userRouter,messageRouter,};
+export { authRouter, userRouter,messageRouter,otpRouter};
