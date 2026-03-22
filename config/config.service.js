aeafc2851119e00a426283aba20c373b21130c78
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-23 00:44:56 +0200

    feat: add role-based token signature handler

--- config/config.service.js
+++ config/config.service.js
@@ -12,8 +12,15 @@ export const PORT = process.env.PORT ?? 7000;
 export const DATABASE_URI = process.env.DATABASE_URI;
 export const SALT = process.env.SALT;
 export const ENCRYPTION_SECRET_KEY = process.env.ENCRYPTION_SECRET_KEY;
+//====user===================================
 export const ACCESS_TOKEN_SECRET_KEY = process.env.ACCESS_TOKEN_SECRET;
 export const REFRESH_TOKEN_SECRET_KEY = process.env.REFRESH_TOKEN_SECRET_KEY;
+//=========admin================================
+export const ADMIN_ACCESS_TOKEN_SECRET_KEY =
+  process.env.ADMIN_ACCESS_TOKEN_SECRET;
+export const ADMIN_REFRESH_TOKEN_SECRET_KEY =
+  process.env.ADMIN_REFRESH_TOKEN_SECRET_KEY;
+
 export const TOKEN_EXPIRES_IN = process.env.TOKEN_EXPIRES_IN;
 export const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN;
 export const MAILTRAP_USER = process.env.MAILTRAP_USER;
--- src/Utils/Security/token.js
+++ src/Utils/Security/token.js
@@ -1,6 +1,12 @@
 import jwt from "jsonwebtoken";
-import { ACCESS_TOKEN_SECRET_KEY, TOKEN_EXPIRES_IN } from "../../../config/config.service.js";
-
+import {
+  ACCESS_TOKEN_SECRET_KEY,
+  ADMIN_ACCESS_TOKEN_SECRET_KEY,
+  ADMIN_REFRESH_TOKEN_SECRET_KEY,
+  REFRESH_TOKEN_SECRET_KEY,
+  TOKEN_EXPIRES_IN,
+} from "../../../config/config.service.js";
+import { SignatureEnum } from "../Enums/user.enums.js";
 
 export const generateToken = (
   payload,
@@ -13,4 +19,21 @@ export const generateToken = (
 
 export const verifyToken = (token, secretKey = ACCESS_TOKEN_SECRET_KEY) => {
   return jwt.verify(token, secretKey);
+};
+export const getSignature = ({ signatureLevel = SignatureEnum.User }) => {
+  let signature = { accessSignature: undefined, refreshSignature: undefined };
+  switch (signatureLevel) {
+    case SignatureEnum.Admin:
+      accessSignature = ADMIN_ACCESS_TOKEN_SECRET_KEY;
+      refreshSignature = ADMIN_REFRESH_TOKEN_SECRET_KEY;
+      break;
+    case SignatureEnum.User:
+      accessSignature = ACCESS_TOKEN_SECRET_KEY;
+      refreshSignature = REFRESH_TOKEN_SECRET_KEY;
+      break;
+    default:
+      accessSignature = ACCESS_TOKEN_SECRET_KEY;
+      refreshSignature = REFRESH_TOKEN_SECRET_KEY;
+  }
+  return signature;
 };
